d1608c392d8c882e9d73e784f7992bd16caa8e63
xich4932 <aminoyls@Gmail.com>
2023-11-06 00:42:06 +0800

    update settings

--- test/test.cpp
+++ test/test.cpp
@@ -11,12 +11,8 @@ using ::testing::TestInfo;
 using ::testing::UnitTest;
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  //EXPECT_STRNE("hello", "world");
-  // Expect equality.
   string got = addBinary("1", "1");
   EXPECT_EQ(got, "10");
-  //EXPECT_EQ(7 * 6, 42);
 }
 
 int main(int argc, char** argv)
