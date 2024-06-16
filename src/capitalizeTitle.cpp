8795b1eda17c87e9c60a2f8d5e53c612335c1100
xich4932 <aminoyls@gmail.com>
2024-06-17 03:27:32 +0800

    update

--- src/capitalizeTitle.cpp
+++ src/capitalizeTitle.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-static vector<string> spliter(string title)
+vector<string> spliter(string title)
 {
     vector<string> ret;
     title += " ";
@@ -12,10 +12,6 @@ static vector<string> spliter(string title)
             j = i + 1;
         }
     }
-    for (auto it : ret) {
-        cout << it << " ";
-    }
-    cout << endl;
     return ret;
 }
 
@@ -25,12 +21,12 @@ string capitalizeTitle(string title)
 
     for (auto& s : ans) {
         if (s.length() > 2) {
-            for (auto& c : s) {
+            for (auto & c : s) {
                 if (c == *s.begin() && c >= 'a' && c <= 'z') {
-                    c = toupper(c);
+                    c += 'A' - 'a';
                 } else {
                     if (c >= 'A' && c <= 'Z') {
-                        c = tolower(c);
+                        c += 'A' - 'a';
                     }
                 }
             }
@@ -41,11 +37,11 @@ string capitalizeTitle(string title)
             }
         }
     }
-    string str = "";
+    string str=  "";
     for (auto c : ans) {
         str += c;
         str += " ";
-        //cout << str << endl;
+    
     }
     return str.substr(str.length() - 1);
 }
